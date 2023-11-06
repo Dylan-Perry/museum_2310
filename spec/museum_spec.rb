342e2d1a386dd1452e1612b6b79fb51551e6bf93
Dylan Perry <dylanbperry@gmail.com>
2023-11-05 19:39:04 -0600

    Add attr_reader and tests for name and exhibits attributes, and default exhibits to empty array

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -8,4 +8,11 @@ describe Museum do
 
         expect(dmns).to be_a Museum
     end
+
+    it "stores and reads name, and creates empty exhibits array" do
+        dmns = Museum.new("Denver Museum of Nature and Science")
+
+        expect(dmns.name).to eq("Denver Museum of Nature and Science")
+        expect(dmns.exhibits).to eq ([])
+    end
 end
