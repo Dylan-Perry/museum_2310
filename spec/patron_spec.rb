faa1befa239d261bbe5a59d9ee9e02b106db1b24
Dylan Perry <dylanbperry@gmail.com>
2023-11-05 19:26:14 -0600

    Add tests to store and read attributes: name, spending money, interests, and default interests to empty array

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -2,8 +2,16 @@ require "./lib/patron"
 
 describe Patron do
     it "exists" do
-        patron = Patron.new
+        patron_1 = Patron.new("Bob", 20)
 
-        expect(patron).to be_a Patron
+        expect(patron_1).to be_a Patron
+    end
+
+    it "stores and reads name, spending money, and interests attributes" do
+        patron_1 = Patron.new("Bob", 20)
+
+        expect(patron_1.name).to eq("Bob")
+        expect(patron_1.spending_money).to eq(20)
+        expect(patron_1.interests).to eq([])
     end
 end
