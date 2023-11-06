a4b3dd311503665099dc12c58f3c6799882e7d36
Dylan Perry <dylanbperry@gmail.com>
2023-11-05 19:32:02 -0600

    Add test for add_interest to add interest string to interests array

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -14,4 +14,15 @@ describe Patron do
         expect(patron_1.spending_money).to eq(20)
         expect(patron_1.interests).to eq([])
     end
+
+    it "adds interests" do
+        patron_1 = Patron.new("Bob", 20)
+        patron_1.add_interest("Dead Sea Scrolls")
+
+        expect(patron_1.interests).to eq(["Dead Sea Scrolls"])
+
+        patron_1.add_interest("Gems and Minerals")
+        
+        expect(patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
+    end
 end
