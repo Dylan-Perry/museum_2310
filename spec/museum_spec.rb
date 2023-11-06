4ee0bcee197b76960e42e0c2583df8e1d2bf26d7
Dylan Perry <dylanbperry@gmail.com>
2023-11-05 19:45:54 -0600

    Refactor add_exhibit test to utilize before :each block

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -16,16 +16,33 @@ describe Museum do
         expect(dmns.exhibits).to eq ([])
     end
 
-    it "adds exhibits" do
-        dmns = Museum.new("Denver Museum of Nature and Science")
-        gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
-        dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
-        imax = Exhibit.new({name: "IMAX",cost: 15})
+    describe "#adds_exhibit and #recommend_exhibit" do
+        before :each do
+            dmns = Museum.new("Denver Museum of Nature and Science")
+            gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+            dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+            imax = Exhibit.new({name: "IMAX",cost: 15})
 
-        dmns.add_exhibit(gems_and_minerals)
-        dmns.add_exhibit(dead_sea_scrolls)
-        dmns.add_exhibit(imax)
+            patron_1 = Patron.new("Bob", 20)
+            patron_1.add_interest("Dead Sea Scrolls")
+            patron_1.add_interest("Gems and Minerals")
 
-        expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+            patron_2 = Patron.new("Sally", 20)
+            patron_2.add_interest("IMAX")
+        end
+
+        it "adds exhibits" do
+            dmns = Museum.new("Denver Museum of Nature and Science")
+            gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+            dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+            imax = Exhibit.new({name: "IMAX",cost: 15})
+    
+            dmns.add_exhibit(gems_and_minerals)
+            dmns.add_exhibit(dead_sea_scrolls)
+            dmns.add_exhibit(imax)
+    
+            expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+        end
+        
     end
 end
