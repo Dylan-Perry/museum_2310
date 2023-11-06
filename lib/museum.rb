9b6eb27554a0db8984939abcb667668c31f23080
Dylan Perry <dylanbperry@gmail.com>
2023-11-05 20:03:54 -0600

    Add recommend_exhibits method and tests

--- lib/museum.rb
+++ lib/museum.rb
@@ -9,4 +9,16 @@ class Museum
     def add_exhibit(exhibit)
         @exhibits << exhibit
     end
+
+    def recommend_exhibits(patron)
+        recommended_exhibits = []
+        patron.interests.map do |interest|
+            @exhibits.map do |exhibit|
+                if exhibit.name.downcase == interest.downcase
+                    recommended_exhibits << exhibit
+                end
+            end
+        end
+        recommended_exhibits
+    end
 end
